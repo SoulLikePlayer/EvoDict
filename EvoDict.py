46fb43379c4eeace5776837a600bd274a32d2738
Louis Lazare <lazare231@etu.unicaen.fr>
2024-04-17 13:54:22 +0200

    [UPDATE -> METHODE POO -> AUTRE METHODE] : Ajout de toute les autre Methode

--- EvoDict.py
+++ EvoDict.py
@@ -76,4 +76,50 @@ class EvoDict:
     def __deepcopy__(self, memo):
         """Renvoie une copie en profondeur du dictionnaire."""
         from copy import deepcopy
-        return EvoDict(deepcopy(self.dictionnaire, memo), self.nom_cle, self.nom_valeur)   
+        return EvoDict(deepcopy(self.dictionnaire, memo), self.nom_cle, self.nom_valeur)  
+    
+    # Autres méthodes de manipulation du dictionnaire
+
+    def keys(self):
+        """Renvoie les clés du dictionnaire."""
+        return self.dictionnaire.keys()
+
+    def values(self):
+        """Renvoie les valeurs du dictionnaire."""
+        return self.dictionnaire.values()
+
+    def items(self):
+        """Renvoie les paires (clé, valeur) du dictionnaire."""
+        return self.dictionnaire.items()
+
+    def update(self, *args, **kwargs):
+        """Met à jour le dictionnaire avec les éléments spécifiés."""
+        self.dictionnaire.update(*args, **kwargs)
+
+    def pop(self, cle, *args):
+        """Supprime et renvoie la valeur associée à la clé spécifiée."""
+        return self.dictionnaire.pop(cle, *args)
+
+    def popitem(self):
+        """Supprime et renvoie une paire (clé, valeur) arbitraire du dictionnaire."""
+        return self.dictionnaire.popitem()
+
+    def clear(self):
+        """Supprime tous les éléments du dictionnaire."""
+        self.dictionnaire.clear()
+
+    def copy(self):
+        """Renvoie une copie superficielle du dictionnaire."""
+        return self.__copy__()
+
+    def deepcopy(self):
+        """Renvoie une copie en profondeur du dictionnaire."""
+        return self.__deepcopy__()
+
+    def get(self, cle, default=None):
+        """Renvoie la valeur associée à la clé spécifiée ou une valeur par défaut si la clé n'existe pas."""
+        return self.dictionnaire.get(cle, default)
+
+    def setdefault(self, cle, default=None):
+        """Renvoie la valeur associée à la clé spécifiée, en l'ajoutant au besoin avec une valeur par défaut."""
+        return self.dictionnaire.setdefault(cle, default) 
