422b8ea5c9825ecec5cdc294cccc7b4db4dd9a48
Louis Lazare <lazare231@etu.unicaen.fr>
2024-04-17 13:18:11 +0200

    [COMMIT 0.1] : Ajout du fichier EvoDict.py

---
+++ EvoDict.py
@@ -0,0 +1,27 @@
+class EvoDict:
+    """
+    Classe représentant un dictionnaire évolué.
+
+    Attributes:
+        dictionnaire (dict): Le dictionnaire contenant les données.
+        nom_cle (str): Le nom utilisé pour désigner les clés.
+        nom_valeur (str): Le nom utilisé pour désigner les valeurs.
+        not_a_key_counter (int): Compteur utilisé pour générer des clés uniques pour les éléments supprimés.
+    """
+
+    def __init__(self, dictionnaire=None, cle="key", valeur="value"):
+        """
+        Initialise un nouvel EvoDict.
+
+        Args:
+            dictionnaire (dict, optional): Le dictionnaire initial. Par défaut, None.
+            cle (str, optional): Le nom à utiliser pour les clés. Par défaut, "key".
+            valeur (str, optional): Le nom à utiliser pour les valeurs. Par défaut, "value".
+        """
+        if dictionnaire is None:
+            self.dictionnaire = dict()
+        else:
+            self.dictionnaire = dictionnaire
+        self.nom_cle = cle
+        self.nom_valeur = valeur
+        self.not_a_key_counter = 0
