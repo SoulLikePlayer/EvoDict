a6f92d7c4b0fa93c151bb2f0983874ffe68adfe6
Louis Lazare <lazare231@etu.unicaen.fr>
2024-04-23 22:33:32 +0200

    [UPDATE] : Modification du fichier ''Test''

--- test.py
+++ test.py
@@ -1,17 +1,15 @@
 from EvoDict import *
+evo_dict = EvoDict({
+        "A": ["B", "C"],
+        "B": "D",
+        "C": "F",
+        "D": [],
+        "E": ["G"],
+        "F": "H",
+        "G": [],
+        "H": "I",
+        "I": []
+    })
 
-def fibonacci(n):
-    """
-    Fonction pour calculer les n premiers termes de la suite de Fibonacci.
-    """
-    fib_dict = EvoDict({"0": 0, "1": 1})  # Initialisation du dictionnaire avec les deux premiers termes
-    for i in range(2, n):
-        fib_dict[str(i)] = fib_dict[str(i-1)] + fib_dict[str(i-2)]  # Calcul des termes suivants
-    return fib_dict
-
-# Test avec les 10 premiers termes de Fibonacci
-n = 10
-fibonacci_dict = fibonacci(n)
-print("Suite de Fibonacci jusqu'au", n, "ème terme")
-fibonacci_dict.Graphe()
-
+    # Visualiser l'arbre
+evo_dict.Graphe()
