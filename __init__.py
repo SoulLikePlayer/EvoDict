b26c78714c7e3959d13cef55633eb84ea6e036c2
Louis Lazare <lazare231@etu.unicaen.fr>
2024-04-24 11:45:19 +0200

    [ERREUR]: recréation du __init__

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .EvoDict import EvoDict
+from .EvoDict import Graphe
+from .EvoDict import FusionError, ExportError, ImportationError
+
+__all__ = ["EvoDict", "Graphe", "FusionError", "ExportError", "ImportationError"]
