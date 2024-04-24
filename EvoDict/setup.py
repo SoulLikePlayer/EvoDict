ddfdd4f67c7a4ba1b554cc3409acf59fcf1c2b58
Louis Lazare <lazare231@etu.unicaen.fr>
2024-04-24 17:44:59 +0200

    [CORRECTION] : Ajout du setup à la racine du projets

--- EvoDict/setup.py
+++ EvoDict/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='EvoDict',
-    version='0.1',
+    version='1.0',
     packages=find_packages(),
     author='Louis',
     author_email='louislazare.pro@gmail.com',
@@ -11,5 +11,17 @@ setup(
         'tabulate',
         'networkx',
         'matplotlib'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
     ],
 )
