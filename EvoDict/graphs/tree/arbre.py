a0318a85f208daba77330d44236ac23c5d982bac
Louis Lazare <lazare231@etu.unicaen.fr>
2024-04-27 22:04:44 +0200

    [UPDATE] : Ajout de la méthode __str__

--- EvoDict/graphs/tree/arbre.py
+++ EvoDict/graphs/tree/arbre.py
@@ -1,5 +1,13 @@
 from EvoDict.graphs import Graphe
 
-class Graphe(Graphe):
+class Arbre(Graphe):
     def __init__(self, dictionnaire=None, cle="key", valeur="value"):
         super().__init__(dictionnaire, cle, valeur)
+    
+    def __setitem__(self, cle, valeur):
+      if (valeur in list(self.dictionnaire.keys())):
+        raise ValueError()
+      return super().__setitem__(cle, valeur)
+     
+    def __str__(self):
+      return super().__str__(self)      
