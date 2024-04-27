2bf85ca3d968233a76437cde495d52a0f336aabf
Louis Lazare <lazare231@etu.unicaen.fr>
2024-04-27 19:49:17 +0200

    [UPDATE] : Création de __init__.py du sous genre Arbre

--- EvoDict/graphs/Arbre/Arbre.py
+++ EvoDict/graphs/Arbre/Arbre.py
@@ -12,34 +12,9 @@ class Arbre(Graphe):
         Empêche la création de plusieurs fils pour un même parent.
         """
         # Vérifie si le nœud fils a déjà un parent
-        if valeur in self.dictionnaire.values():
+        if valeur in self.dictionnaire.keys():
             raise ValueError("Un nœud ne peut avoir qu'un seul parent dans un arbre.")
         super().__setitem__(cle, valeur)
 
     def __str__(self):
-        """Affiche l'arbre sous forme de graphe en évitant les cycles."""
-        G = nx.DiGraph()
-        visited = set()
-
-        def dfs(node, parent=None):
-            if node in visited:
-                return
-            visited.add(node)
-            G.add_node(node)
-            if parent is not None:
-                G.add_edge(parent, node)
-            children = self.dictionnaire.get(node, [])
-            if isinstance(children, list):
-                for child in children:
-                    dfs(child, node)
-            else:
-                dfs(children, node)
-
-        root = next(iter(self.dictionnaire.keys()), None)
-        if root is not None:
-            dfs(root)
-
-        pos = nx.spring_layout(G)
-        nx.draw(G, pos, with_labels=True, arrows=True)
-        plt.show()
-        return ""
+        return super().__str__()
