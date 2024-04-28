91a763acfa311d5ea0465271d1a1a756473d5a75
Louis Lazare <lazare231@etu.unicaen.fr>
2024-04-28 23:57:55 +0200

    [UPDATE] : modification du .gitignore

---
+++ tests/puissance_matrice.py
@@ -0,0 +1,9 @@
+from EvoDict import *
+
+# Création d'une matrice
+matrice = Matrice({"ligne1": [1, 2], "ligne2": [3, 4]})
+
+# Élévation de la matrice à la puissance 2
+resultat_puissance = matrice.puissance(2)
+print("Matrice élevée à la puissance 2 :")
+print(resultat_puissance)
