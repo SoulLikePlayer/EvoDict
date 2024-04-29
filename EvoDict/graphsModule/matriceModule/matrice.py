ebb86c318657aab87abb17a2f73d8ae6aef12009
Louis Lazare <lazare231@etu.unicaen.fr>
2024-04-29 12:13:36 +0200

    [UPDATE -> MATRICE] : ajout de __call__

--- EvoDict/graphsModule/matriceModule/matrice.py
+++ EvoDict/graphsModule/matriceModule/matrice.py
@@ -1,5 +1,6 @@
 from tabulate import tabulate
 from EvoDict.graphsModule import Graphe
+import numpy as np
 
 class Matrice(Graphe):
     def __init__(self, dictionnaire=None, cle="key", valeur="value"):
@@ -96,3 +97,97 @@ class Matrice(Graphe):
             return result
         else:
             raise ValueError("La puissance doit être un entier positif ou nul.")
+
+# Méthode de transposition de la matrice
+    def transposition(self):
+        """
+        Transpose la matrice en échangeant lignes et colonnes.
+        """
+        transposed_dict = {key: [] for key in range(len(list(self.dictionnaire.values())[0]))}
+        for key, values in self.dictionnaire.items():
+            for i, value in enumerate(values):
+                transposed_dict[i].append(value)
+        self.dictionnaire = transposed_dict
+
+    # Méthode de calcul du déterminant de la matrice (uniquement pour les matrices carrées)
+    def determinant(self):
+        """
+        Calcule le déterminant de la matrice (uniquement pour les matrices carrées).
+
+        Returns:
+            float: Le déterminant de la matrice.
+        """
+        if len(self.dictionnaire) != len(list(self.dictionnaire.values())[0]):
+            raise ValueError("Le déterminant n'est défini que pour les matrices carrées.")
+        return np.linalg.det(list(self.dictionnaire.values()))
+
+    # Méthode de calcul de l'inverse de la matrice (uniquement pour les matrices carrées inversibles)
+    def inverse(self):
+        """
+        Calcule l'inverse de la matrice (uniquement pour les matrices carrées inversibles).
+
+        Returns:
+            Matrice: La matrice inverse.
+        """
+        if len(self.dictionnaire) != len(list(self.dictionnaire.values())[0]):
+            raise ValueError("L'inverse n'est défini que pour les matrices carrées.")
+        inverse_array = np.linalg.inv(list(self.dictionnaire.values()))
+        inverse_dict = {i: inverse_array[i].tolist() for i in range(len(inverse_array))}
+        return Matrice(inverse_dict)
+
+    # Méthode de calcul de la trace de la matrice (uniquement pour les matrices carrées)
+    def trace(self):
+        """
+        Calcule la trace de la matrice (uniquement pour les matrices carrées).
+
+        Returns:
+            float: La trace de la matrice.
+        """
+        if len(self.dictionnaire) != len(list(self.dictionnaire.values())[0]):
+            raise ValueError("La trace n'est définie que pour les matrices carrées.")
+        return np.trace(list(self.dictionnaire.values()))
+
+    # Méthode de calcul des valeurs propres et des vecteurs propres (uniquement pour les matrices carrées)
+    def valeurs_vecteurs_propres(self):
+        """
+        Calcule les valeurs propres et les vecteurs propres de la matrice (uniquement pour les matrices carrées).
+
+        Returns:
+            tuple: Un tuple contenant les valeurs propres et les vecteurs propres.
+        """
+        if len(self.dictionnaire) != len(list(self.dictionnaire.values())[0]):
+            raise ValueError("Les valeurs propres et les vecteurs propres ne sont définis que pour les matrices carrées.")
+        eigenvalues, eigenvectors = np.linalg.eig(list(self.dictionnaire.values()))
+        return eigenvalues, eigenvectors        
+    
+    def __call__(self):
+        """
+        Affiche toutes les informations disponibles sur la matrice, y compris ses propriétés mathématiques et son type.
+        """
+        print("Informations sur la matrice :")
+        print("Contenu de la matrice :")
+        print(self.__str__())
+        print("Propriétés mathématiques :")
+        print("Nombre de lignes :", len(self.dictionnaire))
+        print("Nombre de colonnes :", len(list(self.dictionnaire.values())[0]))
+        print("Type de la matrice :", type(self).__name__)
+        print("\nInformations mathématiques:")
+        if len(self.dictionnaire) == len(list(self.dictionnaire.values())[0]):
+            print("Transposée:")
+            self.transposition()
+            print(str(self))
+            print("Déterminant:", self.determinant())
+            print("Inverse:")
+            try :
+                inverse_matrix = self.inverse()
+                print(str(inverse_matrix))
+            except :
+                print("Matrice non inversible")    
+            print("Trace:", self.trace())
+            eigenvalues, eigenvectors = self.valeurs_vecteurs_propres()
+            print("Valeurs propres:", eigenvalues)
+            print("Vecteurs propres:")
+            for i in range(len(eigenvectors)):
+                print("Vecteur propre", i+1, ":", eigenvectors[i])
+        else:
+            print("La transposée, le déterminant, l'inverse, la trace, les valeurs propres et les vecteurs propres ne sont définis que pour les matrices carrées.")
---
+++ tests/test_graphe.py
@@ -0,0 +1,12 @@
+from EvoDict import *
+
+# Création d'une matrice de test
+matrice_test = MatriceBinaire({
+    "ligne 1": [1, 2, 3],
+    "ligne 2": [0, 1, 4],
+    "ligne 3": [5, 6, 10]
+})
+
+
+# Affichage de toutes les informations de la matrice
+matrice_test()
