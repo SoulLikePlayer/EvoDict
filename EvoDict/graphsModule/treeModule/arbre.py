348bd74c9a50a1d6e97263c0d1977f814b4a3fb9
Louis Lazare <lazare231@etu.unicaen.fr>
2024-05-01 12:07:31 +0200

    [DOCUMENTATION] : Graphe.py + Arbr*e.py

--- EvoDict/graphsModule/treeModule/arbre.py
+++ EvoDict/graphsModule/treeModule/arbre.py
@@ -3,13 +3,52 @@ import matplotlib.pyplot as plt
 from EvoDict.graphsModule import Graphe
 
 class Arbre(Graphe):
+    """
+    Classe représentant un arbre, un type spécifique de graphe.
+
+    Attributes:
+        dictionnaire (dict): Le dictionnaire contenant les données de l'arbre.
+        nom_cle (str): Le nom utilisé pour désigner les clés dans le dictionnaire.
+        nom_valeur (str): Le nom utilisé pour désigner les valeurs dans le dictionnaire.
+    """
+
     def __init__(self, dictionnaire=None, cle="key", valeur="value"):
+        """
+        Initialise un nouvel objet de la classe Arbre.
+
+        Args:
+            dictionnaire (dict, optional): Le dictionnaire initial contenant les données de l'arbre. Par défaut, None.
+            cle (str, optional): Le nom à utiliser pour les clés dans le dictionnaire. Par défaut, "key".
+            valeur (str, optional): Le nom à utiliser pour les valeurs dans le dictionnaire. Par défaut, "value".
+        """
+        # Appel du constructeur de la classe parente avec les noms de clé et de valeur appropriés
         super().__init__(dictionnaire, cle, valeur)
-    
+
     def __setitem__(self, cle, valeur):
-      if (valeur in list(self.dictionnaire.keys())):
-        raise ValueError()
-      return super().__setitem__(cle, valeur)
-     
+        """
+        Définit la valeur associée à la clé spécifiée dans le dictionnaire de l'arbre.
+
+        Si la valeur est déjà une clé existante dans le dictionnaire, lève une ValueError.
+
+        Args:
+            cle (str): La clé à définir.
+            valeur (list): La liste des valeurs associées à la clé.
+
+        Raises:
+            ValueError: Si la valeur est déjà une clé existante dans le dictionnaire.
+        """
+        # Vérifie si la valeur est déjà une clé existante dans le dictionnaire
+        if valeur in list(self.dictionnaire.keys()):
+            raise ValueError("La valeur '{}' est déjà une clé existante dans le dictionnaire de l'arbre.".format(valeur))
+        # Appel de la méthode de la classe parente pour définir la valeur associée à la clé spécifiée
+        return super().__setitem__(cle, valeur)
+
     def __str__(self):
-      return super().__str__()      
+        """
+        Affiche le dictionnaire de l'arbre sous forme de graphe dirigé à l'aide du parcours en profondeur.
+
+        Returns:
+            str: Une chaîne vide, car l'affichage du graphe est géré par matplotlib.
+        """
+        # Appel de la méthode de la classe parente pour afficher le dictionnaire sous forme de graphe dirigé
+        return super().__str__()
