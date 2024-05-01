d2f42853b3ceb71c9e64ade6bacf0f54dc6e30ce
Louis Lazare <lazare231@etu.unicaen.fr>
2024-05-01 12:30:42 +0200

    [DOCUMENTATION] : Matrice + sous-genre des matrice

--- EvoDict/graphsModule/matriceModule/matriceTriangulaire/matriceTriangulaire.py
+++ EvoDict/graphsModule/matriceModule/matriceTriangulaire/matriceTriangulaire.py
@@ -1,23 +1,35 @@
 from EvoDict.graphsModule.matriceModule.matrice import Matrice
 
 class MatriceTriangulaire(Matrice):
-    """Représentation d'une matrice triangulaire
-    Attributes:
-        dictionnaire (dict): Le dictionnaire contenant les données.
-        nom_cle (str): Le nom utilisé pour désigner les clés.
-        nom_valeur (str): Le nom utilisé pour désigner les valeurs.
-        type (str) : détermine si le triangle est inférieur ou supérieur
-                    - "inf" pour inférieur
-                    - "sup" pour supérieur
-       
     """
-    def __init__(self, dictionnaire=None, cle="key", valeur="value", type = "inf"):
+    Représentation d'une matrice triangulaire.
+
+    Attributes:
+        dictionnaire (dict): Le dictionnaire contenant les données de la matrice.
+        nom_cle (str): Le nom utilisé pour désigner les clés dans le dictionnaire.
+        nom_valeur (str): Le nom utilisé pour désigner les valeurs dans le dictionnaire.
+        type (str): Détermine si le triangle est inférieur ou supérieur.
+                    - "inf" pour inférieur.
+                    - "sup" pour supérieur.
+    """
+
+    def __init__(self, dictionnaire=None, cle="key", valeur="value", type="inf"):
+        """
+        Initialise un nouvel objet de la classe MatriceTriangulaire.
+
+        Args:
+            dictionnaire (dict, optional): Le dictionnaire initial contenant les données de la matrice. Par défaut, None.
+            cle (str, optional): Le nom de la clé. Par défaut, "key".
+            valeur (str, optional): Le nom de la valeur. Par défaut, "value".
+            type (str, optional): Détermine si le triangle est inférieur ou supérieur. Par défaut, "inf".
+        """
+        # Appeler le constructeur de la classe parente avec les noms de clé et de valeur appropriés
         super().__init__(dictionnaire, cle, valeur)
         # Appeler la méthode rendre_triangulaire seulement si la matrice est initialisée
         if self.dictionnaire is not None:
-            if (type == "inf"):
+            if type == "inf":
                 self.rendre_triangulaire(True)
-            elif (type == "sup"):
+            elif type == "sup":
                 self.rendre_triangulaire(False)    
 
     def rendre_triangulaire(self, inferieure):
