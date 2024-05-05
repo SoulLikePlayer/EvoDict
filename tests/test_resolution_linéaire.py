79cbb3d067e01b15e602f65e010a35f6ce532dcf
Louis Lazare <lazare231@etu.unicaen.fr>
2024-05-05 11:57:05 +0200

    [UPDATE -> MATRICE] : Ajout de nouveau test

--- tests/test_resolution_linéaire.py
+++ tests/test_resolution_linéaire.py
@@ -3,12 +3,12 @@ from EvoDict import *
 # Création de la matrice A
 matrice_a = Matrice({
     'row1': [2, 1, -1],
-    'row2': [-3, -1, 2],
-    'row3': [-2, 1, 2]
+    'row2': [4, -1, 3],
+    'row3': [1, -1, 2]
 })
 
 # Vecteur b
-vecteur_b = [8, -11, -3]
+vecteur_b = [4, 2, -1]
 
 # Résolution du système d'équations linéaires Ax = b
 solution = Resolution(matrice_a).resoudre_systeme_lineaire(vecteur_b)
